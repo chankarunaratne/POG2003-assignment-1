aaa9de38ae47a51add903e4e39d679f7e6d4675d
Chandima Karunaratne <c.karunaratne.mudiyanselage.10@student.scu.edu.au>
2024-07-14 16:27:42 +1000

    added the java and bluej files

---
+++ CalculateMaxMin.java
@@ -0,0 +1,72 @@
+
+/**
+ * A program that takes the input of students' marks and outputs the maximim and minimim score
+ * Author: Chandima karunaratne
+ */
+import java.util.Scanner;
+public class CalculateMaxMin
+{
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] marks = new int[30];
+        int maxMark = 0;
+        int minMark = 30;
+        int counter = 0;
+        String asstName;
+        
+        // getting the assignment name
+        System.out.println("Enter the assignment name");
+        asstName = scanner.nextLine();
+        
+        // loop to get the marks
+        while(counter < 30) {
+        System.out.println("Enter the mark for student " + (counter+1) + " or enter done if you are finished");
+        String input = scanner.nextLine();
+        // check if the user inputs "done"
+        if(input.equalsIgnoreCase("done")) {
+            break;
+        }
+        // convert the input to an integer
+        int mark = -1;
+        try {
+            mark = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Please enter a number");
+        }
+        
+        // validate mark
+        if (mark <0 || mark >30) {
+            System.out.println("Error: please enter a mark between 0 and 30");
+            continue;
+        }
+        
+        // add marks to the array
+        marks[counter] = mark;
+        counter++;
+        
+        //update min and max marks
+        if (mark > maxMark) {
+            maxMark = mark;
+        }
+        
+        if (mark < minMark) {
+            minMark = mark;
+        }
+        
+    }
+    
+    // print the results
+        System.out.println("Assignment name: " + asstName);
+        System.out.println("Marks: ");
+        for (int i=0; i<counter; i++) {
+            System.out.println("Student " + (i+1) + ": " + marks[i]);
+        }
+        System.out.println("Maximum mark is " + maxMark);
+        System.out.println("Minimum mark is " + minMark);
+        
+        //close the scanner
+        scanner.close();
+    
+    }
+}
